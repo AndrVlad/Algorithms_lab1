49e0af5c9f89ef273d31036fb5f2a1c2ec7610f1
Andrianooo <vladosandrianov@yandex.ru>
2024-09-08 10:52:52 +0300

    Добавьте файлы проекта.

---
+++ lab1-vs.cpp
@@ -0,0 +1,222 @@
+﻿
+#include <clocale> 
+#include <iostream>
+#include <string>
+#include "serial.h"
+using namespace std;
+
+int TIME, last_time, elem_num = 0;
+
+struct Detail
+{
+	string kod;
+	int time = 0;
+	//int time;
+};
+
+int main()
+{	
+	setlocale(LC_ALL, "");
+	Detail detail;
+	int detal = 0;
+	int choose = 0;
+	//Queue <int> q;
+	Queue <Detail> q;
+	do
+	{
+		std::cout << "Текущее время: " << TIME << std::endl;
+		std::cout << "last time: " << last_time << std::endl;
+		std::cout << "___________________________________________" << std::endl
+			<< "Выберите действие: " << std::endl
+			<< "1. Постановка детали на обработку" << std::endl
+			<< "2. Следующий момент модального времени" << std::endl
+			<< "3. Снятие детали с обработки до её завершения" << std::endl
+			<< "4. Вывод списка обрабатываемых деталей в порядке очереди " << std::endl
+			<< "5. Сброс процесса моделирования" << std::endl
+			<< "______________________________________________" << std::endl;
+		std::cin >> choose;
+				switch (choose)
+				{
+				case 1:
+				{
+					cout << "Введите код детали и время её обработки" << endl;
+
+					cin >> detail.kod >> detail.time;
+					//cin >> detal;
+
+					if (q.Enqueue(detail)) {
+						cout << "Деталь добавлена";
+					}
+					else {
+						cout << "Очередь полна!";
+					};
+
+					cout << "head = " << q.head << "tail = " << q.tail;
+
+					break;
+				}
+				case 2:
+				{
+					TIME++;
+					
+					if (q.getDetail(detail)) {
+						
+						if (TIME == (detail.time + last_time)) {
+							q.Dequeue(detail);
+							last_time += detail.time;
+							cout << "Деталь " << detail.kod << " была обработана" << endl;
+						}
+					}
+					else {
+						last_time = TIME;
+					}
+
+					/*if (q.Dequeue(detail)) {
+						cout << "Деталь удалена";
+					}
+					else {
+						cout << "Пусто";
+					};
+
+					cout << "head = " << q.head << "tail = " << q.tail;*/
+					break;
+				}
+				case 3:
+				{
+					if (!(q.Dequeue(detail))) {
+						cout << "Очередь пуста!" << endl;
+					}
+					else {
+						TIME = last_time = 0;
+						cout << "Отказ установки! " << " Деталь " << detail.kod << " была снята" << endl;
+					}
+
+					break;
+				}
+				case 4:
+				{
+					if (!(q.ElemCount())) {
+						cout << "Очередь пуста!" << endl;
+					}
+					else {
+						elem_num = q.ElemCount();
+						for (int i = 0; i < elem_num; i++) {
+							q.getDetail(detail, i);
+							cout << " <- " << detail.kod << endl;
+						}
+					}
+
+					break;
+				}
+				case 5:
+				{
+					TIME = last_time = 0;
+					q.InitQueue();
+					break;
+				}
+				default:
+					cout << "Ошибка! Попробуйте ввести другое число." << endl;
+				}
+			} while (true);
+
+	return 0;
+}
+
+/*
+
+#include <clocale> 
+#include <iostream>
+#include <string>
+using namespace std;
+#include "serial.h"
+//#include "svyaznaya.h"
+
+struct Detail
+{
+	string kod;
+	int vremya_obrabotki;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int choose;
+	int model_time = 0;
+	Detail detail;
+	Queue<Detail> ochered;
+	do
+	{
+		cout << "___________________________________________" << endl
+			<< "Выберите действие: " << endl
+			<< "1. Постановка детали на обработку" << endl
+			<< "2. Переход к следующему моменту модельного времени" << endl
+			<< "3. Снятие детали с обработки до её завершения" << endl
+			<< "4. Вывод списка обрабатываемых деталей в порядке очереди " << endl
+			<< "5. Сброс процесса моделиования" << endl
+			<< "______________________________________________" << endl;
+		cin >> choose;
+		switch (choose)
+		{
+		case 1:
+		{
+			cout << "Введите код детали и время её обработки" << endl;
+			cin >> detail.kod >> detail.vremya_obrabotki;
+			if (ochered.enQueue(detail))
+				cout << "Деталь успешно добавлена" << endl;
+			else
+				cout << "Ошибка, очередь переполнена" << endl;
+			break;
+		}
+		case 2:
+		{
+			model_time++;
+			if (!ochered.isEmpty())
+			{
+				ochered.retFirst(detail);
+				if (detail.vremya_obrabotki == model_time)
+				{
+					ochered.deQueue(detail);
+					model_time = 0;
+					cout << "Обработка детали успешно завершена" << endl;
+				}
+			}
+			break;
+		}
+		case 3:
+		{
+			if (ochered.deQueue(detail))
+			{
+				model_time = 0;
+				cout << "Обработка детали аварийно завершена " << endl;
+			}
+			else
+				cout << "Ошибка, очередь пуста" << endl;
+			break;
+		}
+		case 4:
+		{
+			int count = ochered.retN();
+			while (count-- > 0)
+			{
+				ochered.deQueue(detail);
+				cout << detail.kod << ' ';
+				ochered.enQueue(detail);
+			}
+			cout << endl;
+			break;
+		}
+		case 5:
+		{
+			while (!ochered.isEmpty())
+			{
+				ochered.deQueue(detail);
+			}
+			model_time = 0;
+			break;
+		}
+		default:
+			cout << "Ошибка! Попробуйте ввести другое число." << endl;
+		}
+	} while (true);
+}
+*/
---
+++ serial.h
@@ -0,0 +1,237 @@
+#pragma once
+#include <iostream>
+using namespace std;
+const int NumElem = 6;
+int ptr;
+
+template<class ElType>
+class Queue
+{
+private:
+	ElType details[NumElem];
+
+public:
+
+	int head, tail;
+
+	Queue() {
+		head = tail = 0;
+	}
+
+	void InitQueue() {
+		head = tail = 0;
+	}
+
+	bool Empty() {
+		if (head == tail) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool Full() {
+		if ((head == 0 && tail == NumElem - 1) || (tail == head - 1)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool Dequeue(ElType x) {
+		if (Empty()) {
+			return false;
+		}
+		else {
+			head = (head + 1) % NumElem;
+			return true;
+		}
+	}
+
+	bool Enqueue(ElType x) {
+		if (Full()) {
+			return false;
+		}
+		else if (Empty()) {
+			details[head] = x;
+			tail = (tail + 1) % NumElem;
+		}
+		else {
+			details[tail] = x;
+			//tail = (tail % (NumElem - 1)) + 1;
+			tail = (tail + 1) % NumElem;
+
+		}
+
+		return true;
+	}
+
+	bool ShowQueue() {
+		if (Empty()) {
+			return false;
+		}
+		else {
+			ptr = head;
+			while (ptr != tail) {
+				//cout << " -> " << details[ptr];
+				cout << " -> " << details[ptr].kod;
+				ptr = (ptr + 1) % NumElem;
+			}
+
+		}
+		return true;
+	}
+
+
+
+	int ElemCount() {
+
+		if (Empty()) {
+			return false;
+		}
+
+		int count = 0;
+		int ptr = head;
+		
+		while (ptr != tail) {
+			ptr = (ptr + 1) % NumElem;
+			count++;
+		}
+
+		return count;
+	}
+
+	bool getDetail(ElType &x) {
+		if (Empty()) {
+			return false;
+		}
+
+		x = details[head];
+		return true;
+	}
+
+	bool getDetail(ElType& x, int offset) {
+			
+		int ptr = (head + offset) % NumElem;
+		x = details[ptr];
+
+		return true;
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+#ifndef _POSLEDOVATELNAYA
+#define _POSLEDOVATELNAYA
+#include <iostream>
+using namespace std;
+#define MaxEl 6
+
+
+template<class ElType>
+class Queue
+{
+	ElType element[MaxEl];
+	int head;
+	int tail;
+
+public:
+	Queue()
+	{
+		head = 0;
+		tail = 0;
+	}
+
+	bool isEmpty()
+	{
+		return head == tail;
+	}
+
+	bool isFull()
+	{
+		return head == (tail + 1) % MaxEl;
+	}
+
+	bool enQueue(ElType x)
+	{
+		if (isFull())
+			return false;
+		else
+		{
+			element[tail] = x;
+			tail = (tail + 1) % MaxEl;
+			return true;
+		}
+	}
+
+	bool deQueue(ElType& x)
+	{
+		if (retFirst(x))
+		{
+			head = (head + 1) % MaxEl;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool retFirst(ElType& x)
+	{
+		if (isEmpty())
+		{
+			cout << "Error 1 - queue is empty!" << endl;
+			return false;
+		}
+		else
+		{
+			x = element[head];
+			return true;
+		}
+	}
+
+	int retN()
+	{
+		if (tail > head)
+			return tail - head;
+		else
+			return (tail - 0) + (MaxEl - head);
+	}
+};
+#endif
+*/
+/*
+bool ShowQueue() {
+		if (Empty()) {
+			return false;
+		}
+		else {
+			ptr = head;
+			while (ptr != tail) {
+				//cout << " -> " << details[ptr];
+				cout << " -> " << details[ptr].kod;
+				ptr = (ptr + 1) % NumElem;
+			}
+
+		}
+		return true;
+	}
+
+
+*/
